43313c27b64dfb85228cb8e1a2eac157a4567010
galitskye <galitsky.e.o@gmail.com>
2016-07-26 19:09:43 +0300

    Add browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,42 @@
-var gulp = require('gulp');
+var gulp 				 = require('gulp'),
+		sass 				 = require('gulp-sass'),
+		browserSync = require('browser-sync');
 
+
+
+//====================================COMPILE_SASS_FILES
+gulp.task('sass', function(){
+	return gulp.src(['app/scss/modules/*.scss', 'app/scss/assets/_*.scss' ])
+				 .pipe(sass())
+				 .pipe(gulp.dest('app/css'))
+				 .pipe(browserSync.reload({stream:true}))
+});
+//====================================COMPILE_SASS_FILES_END
+
+//====================================BROWSER_SYNC
+gulp.task('browser-sync', function(){
+	browserSync({
+		port: 9990,
+		server: {
+			baseDir: 'app'
+		},
+		notify: false
+	});
+});
+//====================================BROWSER_SYNC_END
+
+//====================================WATCH
+gulp.task('watch', ['browser-sync','sass'], function(){
+	gulp.watch(['app/scss/modules/*.scss', 'app/scss/assets/_*.scss' ],['sass']);
+	gulp.watch(['app/*.html','app/layouts/*.html'],browserSync.reload);
+	gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+//====================================WATCH_END
+
+//====================================DEFAULT_GULP_TASK
 gulp.task('default', function() {
-
-})
+	return gulp.src('Where we take')
+				 .pipe(plugin())
+				 .pipe(gulp.dest('Where we brought'))
+});
+//====================================DEFAULT_GULP_TASK_END
