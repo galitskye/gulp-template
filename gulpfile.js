a4688b9f7833135490ee4891d80b6e08cae82d74
galitskye <galitsky.e.o@gmail.com>
2016-07-27 21:04:23 +0300

    Empty commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,44 @@
 var gulp 				= require('gulp'),
 		sass 				= require('gulp-sass'),
 		browserSync = require('browser-sync'),
+		reload      = browserSync.reload,
 		useref      = require('gulp-useref'),
 		gulpif			= require('gulp-if'),
 		uglify      = require('gulp-uglify'),
 		minifyCss   = require('gulp-minify-css'),
 		rename      = require('gulp-rename');
 
-
+var path = {
+  src: {
+  	html: 'app/*.html',
+  	js: 'app/js/*.js',
+    style: 'app/sass/module/*.scss',
+    img: 'app/images/**/*.*',
+    fonts: 'app/fonts/**/*.*'    
+  },
+  dist : {
+    html: 'dist',
+  	js: 'dist/js',
+    style: 'dist/css',
+    img: 'dist/images/',
+    fonts: 'dist/fonts/'
+  },
+  watch: {
+    html: 'app/**/*.html',
+    js: 'app/js/**/*.js',
+    style: 'app/sass/**/*.scss',
+    img: 'app/images/**/*.*',
+    fonts: 'app/fonts/**/*.*'     
+  },
+  clean: './build'
+};
 
 //====================================COMPILE_SASS_FILES
 gulp.task('sass', function(){
 	return gulp.src(['app/scss/modules/*.scss', 'app/scss/assets/_*.scss' ])
 		  	 .pipe(sass())
 		  	 .pipe(gulp.dest('app/css'))
-		  	 .pipe(browserSync.reload({stream:true}))
+		  	 .pipe(reload({stream:true}))
 });
 //====================================COMPILE_SASS_FILES_END
 
@@ -33,8 +57,8 @@ gulp.task('browser-sync', function(){
 //====================================WATCH
 gulp.task('watch', ['browser-sync','sass'], function(){
 	gulp.watch(['app/scss/modules/*.scss', 'app/scss/assets/_*.scss' ],['sass']);
-	gulp.watch(['app/*.html','app/layouts/*.html'],browserSync.reload);
-	gulp.watch('app/js/**/*.js', browserSync.reload);
+	gulp.watch(['app/*.html','app/layouts/*.html'],reload);
+	gulp.watch('app/js/**/*.js', );
 });
 //====================================WATCH_END
 
@@ -47,23 +71,29 @@ gulp.task('default', function() {
 //====================================DEFAULT_GULP_TASK_END
 
 //====================================BUILD_END
-gulp.task('userefConcat', function () {
+gulp.task('useref:build', function () {
     return gulp.src('app/*.html')
         .pipe(useref())
-        // .pipe(gulpif('*.js', uglify()))
-        // .pipe(gulpif('*.css', minifyCss()))
         .pipe(gulp.dest('dist'));
 });
-gulp.task('scripts', function () {
-    return gulp.src('dist/main.html')
-        .pipe(useref())
-        // .pipe(gulpif('*.js', uglify()))
-        // .pipe(gulpif('*.css', minifyCss()))
+gulp.task('scripts:build', function () {
+    return gulp.src('dist/js/*.js')
+    		.pipe(uglify())
+    		.pipe(rename(function(path){
+        	path.dirname += "/js";
+			    path.basename += "";
+			    path.extname = ".min.js"
+        }))
         .pipe(gulp.dest('dist'));
-    return gulp.src('dist/main.html')
-        .pipe(useref())
-        // .pipe(gulpif('*.js', uglify()))
-        // .pipe(gulpif('*.css', minifyCss()))
+});
+gulp.task('styles:build', function () {
+    return gulp.src('dist/css/*.css')
+        .pipe(minifyCss())
+        .pipe(rename(function(path){
+        	path.dirname += "/css";
+			    path.basename += "";
+			    path.extname = ".min.css"
+        }))
         .pipe(gulp.dest('dist'));
 });
 //====================================BUILD_END
