d90e3ba9fe4e0f34680019d0da698ea9762ea04e
galitskye <galitsky.e.o@gmail.com>
2016-07-30 15:46:04 +0300

    Finish work

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,108 @@
 var gulp 				   = require('gulp'),
 		sass 				   = require('gulp-sass'),
+    cssComb        = require('gulp-csscomb'),
 		browserSync    = require('browser-sync'),
 		reload         = browserSync.reload,
-		useref         = require('gulp-useref'),
-		gulpif			   = require('gulp-if'),
+    useref         = require('gulp-useref'),
+		gulpif         = require('gulp-if'),
 		uglify         = require('gulp-uglify'),
 		minifyCss      = require('gulp-minify-css'),
 		rename         = require('gulp-rename'),
     rigger         = require('gulp-rigger'),
     prettify       = require('gulp-html-prettify'),
     autoprefixer   = require('gulp-autoprefixer'),
-    sourcemaps     = require('gulp-sourcemaps');
-
-//====================================COMPILE_SASS_FILES
-gulp.task('sass', function(){
-	return gulp.src(['app/scss/modules/*.scss', 'app/scss/assets/_*.scss' ])
-		  	 .pipe(sass().on('error', sass.logError))
-         .pipe(sourcemaps.init())
-         .pipe(autoprefixer({
-            browsers: ['last 150 versions','ie 8'],
-            cascade: false
-          }))
-         .pipe(sourcemaps.write())
-		  	 .pipe(gulp.dest('app/css'))
-		  	 .pipe(reload({stream:true}))
-});
-//====================================COMPILE_SASS_FILES_END
-
-//====================================BROWSER_SYNC
-gulp.task('browser-sync', function(){
-	browserSync({
-		port: 9990,
-		server: {
-			baseDir: 'app'
-		},
-		notify: false
-	});
-});
-//====================================BROWSER_SYNC_END
+    sourcemaps     = require('gulp-sourcemaps'),
+    imageMin       = require('gulp-imagemin'),
+    wiredep        = require('wiredep').stream,
+    jsValidate     = require('gulp-jsvalidate'),
+    notify         = require('gulp-notify'),
+    plumber        = require('gulp-plumber');
 
 //====================================HTML_TASK
   gulp.task('rigger', function () {
       gulp.src('app/pages/*.html')
           .pipe(rigger())
-          .pipe(prettify())
           .pipe(gulp.dest('app/'))
-          .pipe(reload({stream: true})); 
+          .pipe(reload({stream:true}));
+  });
+  gulp.task('bower', function () {
+    gulp.src(['app/modules/head-settings.html','app/modules/scripts-add.html'])
+        .pipe(wiredep({
+          directory : "app/libs"
+        }))
+        .pipe(gulp.dest('app/modules'));
   });
 //====================================HTML_TASK_END
 
+//====================================COMPILE_SASS_FILES
+gulp.task('sass', function(){
+  return gulp.src(['app/scss/modules/*.scss', 'app/scss/assets/_*.scss' ])
+         .pipe(sourcemaps.init())
+         .pipe(sass())
+         .on('error', notify.onError(function(err){
+            return {
+              title: 'Styles',
+              message: err.message
+            };
+           }))
+         .pipe(autoprefixer({
+            browsers: ['last 150 versions','ie 8'],
+            cascade: false
+          }))
+         .pipe(cssComb())
+         .pipe(sourcemaps.write())
+         .pipe(gulp.dest('app/css'))
+         .pipe(reload({stream:true}));
+});
+//====================================COMPILE_SASS_FILES_END
+
+//====================================COMPILE_JAVASCRIPT_FILES
+  gulp.task('scripts', function () {
+    gulp.src('app/js/*.js')
+        .pipe(plumber({
+          errorHendler: notify.onError(function(err){
+            return {
+              title: 'JavaScript',
+              message: err.message
+            };
+           })
+        }))
+        .pipe(jsValidate())
+        .on('error', notify.onError())
+        .pipe(reload({stream:true}));
+  });
+//====================================COMPILE_JAVASCRIPT_FILES_END
+
+//====================================BROWSER_SYNC
+gulp.task('browser-sync', function(){
+  browserSync({
+    port: 9990,
+    server: {
+      baseDir: 'app'
+    },
+    notify: false
+  });
+});
+//====================================BROWSER_SYNC_END
+
 //====================================WATCH
-  gulp.task('watch', ['rigger','sass','browser-sync'], function(){
+  gulp.task('default', ['bower','sass'], function(){
   	gulp.watch(['app/scss/modules/*.scss', 'app/scss/assets/_*.scss' ],['sass']);
   	gulp.watch(['app/pages/*.html','app/modules/*.html'],['rigger']);
-  	gulp.watch(['app/js/**/*.js'],[reload]);
+    gulp.watch(['app/js/**/*.js'],['scripts']);
+  	gulp.watch(['bower.json'],['bower']);
+    gulp.start('browser-sync');
+    gulp.start('rigger');
   });
 //====================================WATCH_END
 
-//====================================DEFAULT_GULP_TASK
-  gulp.task('default', function() {
-  	return gulp.src('Where we take')
-  				 .pipe(plugin())
-  				 .pipe(gulp.dest('Where we brought'))
-  });
-//====================================DEFAULT_GULP_TASK_END
-
 //====================================BUILD_END
-  gulp.task('useref:build', function () {
+  gulp.task('html:build',['useref'], function () {
+      return gulp.src('dist/*.html')
+          .pipe(prettify({indent_size: 2}))
+          .pipe(gulp.dest('dist'));
+  });
+  gulp.task('useref', function () {
       return gulp.src('app/*.html')
           .pipe(useref())
           .pipe(gulp.dest('dist'));
@@ -89,8 +125,14 @@ gulp.task('browser-sync', function(){
           }))
           .pipe(gulp.dest('dist'));
   });
-  gulp.task('build',['useref:build'], function(){ //==============BILD_ALL_PROJECT
+  gulp.task('imageMin:build', function(){
+    gulp.src('app/images/*')
+        .pipe(imageMin())
+        .pipe(gulp.dest('dist/images'))
+  });
+  gulp.task('build',['html:build'], function(){ //==============BILD_ALL_PROJECT
     gulp.start('scripts:build');
     gulp.start('styles:build');
+    gulp.start('imageMin:build');
   });
 //====================================BUILD_END
